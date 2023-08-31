5168e6d286278564067e60d0dc0d606151dfc5ab
Shashmit kumar <shaashmit@gmail.com>
2023-09-01 02:47:01 +0530

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+//const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set("X-RapidAPI-Key", "d9341d9dbdmshdcfc3dfa675140cp16dc8djsn1f025cebc239");
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
